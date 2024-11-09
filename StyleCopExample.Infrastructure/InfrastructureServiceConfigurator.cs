9af10a419a88fa80f2e8bcb13bf100e445997150
patrikduch <duchpatrik@icloud.com>
2024-11-09 16:14:08 +0100

    feat: InfrastructureServicesConfigurator

---
+++ StyleCopExample.Infrastructure/InfrastructureServiceConfigurator.cs
@@ -0,0 +1,19 @@
+﻿namespace StyleCopExample.Infrastructure;
+
+using Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Registration of infrastructure services.
+/// </summary>
+public static class InfrastructureServiceConfigurator
+{
+    /// <summary>
+    /// Definition of service sets that are being used by Infrastructure project.
+    /// </summary>
+    /// <param name="services">Service collection fo Dependency Injection Container.</param>
+    /// <returns>Collection of DI services.</returns>
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+    {
+        return services;
+    }
+}
